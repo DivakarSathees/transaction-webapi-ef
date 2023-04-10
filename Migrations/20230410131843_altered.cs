9d4c939960943a090b3e9ce4c0e730692d768e31
divakar <divakarsathees7@gmail.com>
2023-04-10 13:30:23 +0000

    transaction webapi ef push

---
+++ Migrations/20230410131843_altered.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Transaction.Migrations
+{
+    public partial class altered : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TransactionTime",
+                table: "Transaction");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "TransactionTime",
+                table: "Transaction",
+                type: "time(0)",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+        }
+    }
+}
